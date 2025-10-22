6f365ac7ec370d58477d3ad6ae778a892ab16468
james Hart <james.hart@digital.justice.gov.uk>
2025-10-22 16:50:57 +0100

    feat/HEAT-962 add an updated health monitor class (#7)
    * test Flask-based health server
    
    * better naming of service
    
    * better hostname for local running
    
    * tidy up the info feed
    
    * 404 message
    
    * quick documentation update
    
    * added docco to health

--- src/hmpps/__init__.py
+++ src/hmpps/__init__.py
@@ -1,6 +1,5 @@
 # Re-export the stable surface
 from .clients.github import GithubSession
-from .clients.health import HealthHttpRequestHandler, HealthServer
 from .clients.service_catalogue import ServiceCatalogue
 from .clients.circleci import CircleCI
 from .clients.slack import Slack
@@ -12,11 +11,11 @@ from .models.repository_info import (
 )
 from .models.alertmanager import AlertmanagerData
 from .services import job_log_handling
+from .services.health_server import HealthServer
 from .utils.utilities import update_dict, fetch_yaml_values_for_key, find_matching_keys
 
 __all__ = [
   'GithubSession',
-  'HealthHttpRequestHandler',
   'HealthServer',
   'ServiceCatalogue',
   'CircleCI',
